0174594160da377c0ac435f7bf97220072ff6235
saikat1016 <saikat1016@gmail.com>
2019-07-21 11:30:48 +0600

    Initial Commit

---
+++ IosTabs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IosTabs
+//
+//  Created by Flora PC on 21/7/19.
+//  Copyright © 2019 Green Appers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
